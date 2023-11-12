7ac3526d4251ff0b2877412a4c652ee5de1e3438
baekseungchan1117 <bsc7386@gmail.com>
2023-11-12 19:21:36 +0900

    token 검증

--- src/main/java/com/qortmdcks/springboot3_jwt_security/config/JwtService.java
+++ src/main/java/com/qortmdcks/springboot3_jwt_security/config/JwtService.java
@@ -61,6 +61,20 @@ public class JwtService {
 
     }
 
+//    토큰 검증
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()));
+    }
+
+    private boolean isToenValid(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public  Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
