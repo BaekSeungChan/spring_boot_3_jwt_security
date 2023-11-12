c13e8a01e0552b3bd659e881bd66061591018dd8
baekseungchan1117 <bsc7386@gmail.com>
2023-11-12 18:24:33 +0900

    first commit

---
+++ src/main/java/com/qortmdcks/springboot3_jwt_security/Springboot3JwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.qortmdcks.springboot3_jwt_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3JwtSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3JwtSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qortmdcks/springboot3_jwt_security/Springboot3JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qortmdcks.springboot3_jwt_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3JwtSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
